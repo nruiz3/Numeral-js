277d7c6722205586921613bc5c68f7f0091b93e3
nruiz <nruiz@coursetrends.com>
2016-01-08 17:19:40 -0600

    Added to languages file.

--- languages.js
+++ languages.js
@@ -987,3 +987,40 @@
         this.numeral.language('uk-UA', language);
     }
 }());
+/*! 
+ * numeral.js language configuration
+ * language : English-Ireland (en-ie)
+ */
+(function () {
+    var language = {
+        delimiters: {
+            thousands: ',',
+            decimal: '.'
+        },
+        abbreviations: {
+            thousand: 'k',
+            million: 'm',
+            billion: 'b',
+            trillion: 't'
+        },
+        ordinal: function (number) {
+            var b = number % 10;
+            return (~~ (number % 100 / 10) === 1) ? 'th' :
+                (b === 1) ? 'st' :
+                (b === 2) ? 'nd' :
+                (b === 3) ? 'rd' : 'th';
+        },
+        currency: {
+            symbol: '€'
+        }
+    };
+
+    // Node
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = language;
+    }
+    // Browser
+    if (typeof window !== 'undefined' && this.numeral && this.numeral.language) {
+        this.numeral.language('en-ie', language);
+    }
+}());
